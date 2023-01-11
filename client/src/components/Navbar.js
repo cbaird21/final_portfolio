977e37755a19edc3125deecaaed7a1e6ac47b342
unknown <chandler.baird21@gmail.com>
2023-01-10 19:35:29 -0700

    created aboutme.js component

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,6 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import AboutMe from './Aboutme'
 
 export default function CollapsibleExample() {
     return (
@@ -11,23 +12,18 @@ export default function CollapsibleExample() {
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link href="#aboutMe">About Me</Nav.Link>
-                        <NavDropdown title="pojects" id="collasible-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">National Park Finder</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Krime Kermitters Hangman
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Project 3</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">
-                                More Projects
-                            </NavDropdown.Item>
-                        </NavDropdown>
+                        <Nav.Link href="#Home">Home</Nav.Link>
+                        <Nav.Link href="#AboutMe">About Me</Nav.Link>
+                        <Nav.Link href="#work">Work</Nav.Link>
                     </Nav>
                     <Nav>
-                        <Nav.Link eventKey={2} href="#contact">
-                            Contact Me
-                        </Nav.Link>
+                        <NavDropdown title="Contact Me" id="collasible-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Linked In</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">
+                                GitHub
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Email</NavDropdown.Item>
+                        </NavDropdown>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
