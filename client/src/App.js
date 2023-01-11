94396dff8b0e5d4c3a15e0e636e478ef37d359ae
unknown <chandler.baird21@gmail.com>
2023-01-10 20:03:15 -0700

    imported about me and work into app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
-
+import Aboutme from './components/Aboutme';
+import Work from './components/Work';
 
 function App() {
   return (
     <div className="App">
       <header className="Navbar">
-        <Navbar/>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Navbar />
       </header>
+      <>
+        <Aboutme />
+      </>
+      <>
+        <Work />
+      </>
     </div>
   );
 }
