d2182c3bc3bd888f843a25bb1f168b9f600847c1
unknown <chandler.baird21@gmail.com>
2023-01-12 18:05:17 -0700

    redid my about me to better reflect. Added image to files

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -1,11 +1,20 @@
-import Card from 'react-bootstrap/Card';
+import image from "client/public/About-me-image.jpg";
 
 export default function About() {
     return (
-        <Card>
-            <Card.Body>I've worked in restaurant management for twelve years now. I'm taking
-                a step towards a career change in hopes of achieving a higher quality
-                of life. I'm excited to start my coding journey!</Card.Body>
-        </Card>
+        <div className="container-fluid">
+            <section>
+                <img className="col-lg-2 mt-5 pfp" src={image} alt="Pfp"></img>
+                <h2>About Me</h2>
+                <p>
+                    I've worked in restaurant management for twelve years now. I'm taking
+                    a step towards a career change in hopes of achieving a higher quality
+                    of life. I'm excited to start my coding journey!
+                </p>
+                <p>
+                    I recently learned and am continuing learning about the M.E.R.N stack as my primary focus. I've also become aquanted to JavaScript, CSS, HTML, MySql and more. I'm looking forward to connecting with you!
+                </p>
+            </section >
+        </div>
     );
 }
--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -7,13 +7,13 @@ export default function CollapsibleExample() {
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
             <Container>
-                <Navbar.Brand Link to="/">Chandler Baird Portfolio</Navbar.Brand>
+                <Navbar.Brand >Chandler Baird Portfolio</Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link to="/">Home</Nav.Link>
-                        <Nav.Link to="/aboutme">About Me</Nav.Link>
-                        <Nav.Link to="/Project" >Work</Nav.Link>
+                        <Link to="/">Home</Link>
+                        <Link to="/About">About</Link>
+                        <Link to="/Project" >Work</Link>
                     </Nav>
                     <Nav>
                         <Nav.Link href="https://github.com/cbaird21" target="blank">Github</Nav.Link>
