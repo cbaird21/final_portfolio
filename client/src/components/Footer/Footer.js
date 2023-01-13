b6d27b7c3131484d20edc973fe128b61d573512e
unknown <chandler.baird21@gmail.com>
2023-01-12 18:18:17 -0700

    input footer data

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,39 @@
+export default function Footer() {
+    return (
+        <div className="container-fluid">
+            <div className="row">
+                <footer className="align-items-center text-center text-white bg-dark col-lg-12 pt-2">
+                    <div className="container">
+                        <section className="mb-2">
+                            <a
+                                href="https://github.com/cbaird21"
+                                role="button"
+                                target="_blank"
+                                rel="noreferrer"
+                            >
+                                <h3>GitHub</h3>
+                            </a>
+                            <a
+                                href="https://www.linkedin.com/in/chandler-baird-39965b129/"
+                                role="button"
+                                rel="noreferrer"
+                                target="_blank"
+                            >
+                                <h3>LinkedIn</h3>
+                            </a>
+                            <a
+                                href="mailto: chandler.barid21@gmail.com"
+                                rel="noreferrer"
+                                role="button"
+                                target="_blank"
+                            >
+                                <h3>Email</h3>
+                            </a>
+                        </section>
+                    </div>
+                </footer>
+            </div>
+        </div>
+    );
+}
+
