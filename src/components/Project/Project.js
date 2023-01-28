50dc99fc485bf03d3dea5d6e07cc6c69ef3dc809
unknown <chandler.baird21@gmail.com>
2023-01-27 23:12:41 -0700

    all links to projects and github repos are now working

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -12,100 +12,137 @@ import ListGroup from 'react-bootstrap/ListGroup';
 
 export default function Project() {
     return (
-        <Link to="/Project" >
-            <section id="work" className="section">
-                <h2>Work</h2>
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src={frogr}
-                        alt="load out of website deployed" />
-                    <Card.Body>
-                        <Card.Title>Frogr</Card.Title>
-                        <Card.Text>
-                            This is a project I worked on with others to create a social media application.
-                        </Card.Text>
-                    </Card.Body>
-                    <ListGroup className="list-group-flush">
-                        <ListGroup.Item>Tech Used: </ListGroup.Item>
-                        <ListGroup.Item>I worked on the front end for this project using React and React bootstrap with custom Sass a collegue created.</ListGroup.Item>
-                        <ListGroup.Item>Contributers:</ListGroup.Item>
-                    </ListGroup>
-                    <Card.Body>
-                        <Card.Link href="https://frogr-app.herokuapp.com/">Site</Card.Link>
-                        <Card.Link href="https://github.com/cbaird21/frogr">Github</Card.Link>
-                    </Card.Body>
-                </Card>
-                <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src={frogger}
-                        alt="load out of website deployed" />
-                    <Card.Body>
-                        <Card.Title>Hangman Game Application</Card.Title>
-                        <Card.Text>
-                            This is a project I worked on with others to recreate our childhood game of Hangman. See if you can win in the Wild West!
-                        </Card.Text>
-                    </Card.Body>
-                    <ListGroup className="list-group-flush">
-                        <ListGroup.Item>Tech Used: </ListGroup.Item>
-                        <ListGroup.Item>I worked on the front end for this project and also worked as the scrum master utilizing the kanban board.</ListGroup.Item>
-                        <ListGroup.Item>Contributers:</ListGroup.Item>
-                    </ListGroup>
-                    <Card.Body>
-                        <Card.Link href="https://crimekermitterhangman.herokuapp.com/">Site</Card.Link>
-                        <Card.Link href="https://github.com/cbaird21/Hangman_app">Github</Card.Link>
-                    </Card.Body>
-                </Card>
-
-                <div className="card text-decoration-none">
-                    <h3>National Park Finder with directions</h3>
-                    <a
-                        href="https://heyitsbradley.github.io/National-Park-Finder-with-Directions/"
-                        target="no_blank"
-                    >
-                        <img className="col-lg-2 mt-5 park"
-                            src={park}
-                            alt="load out of website deployed"
-                        /></a>
-                </div>
-                <div className="card text-decoration-none">
-                    <h3> Readme Generator </h3>
-                    <a
-                        href="https://drive.google.com/file/d/1kmY9v3tVTNeo5AJn9NFcYaTgATlaDoSU/view"
-                        target="no_blank"
-                    ><img className="col-lg-2 mt-5 readMe"
-                        src={readMe}
-                        alt="load out of readme.md file"
-                        /></a>
-                </div>
-                <div className="card text-decoration-none">
-                    <h3> Note Taker </h3>
-                    <a
-                        href="https://nottaker1.herokuapp.com/"
-                        target="no_blank"
-                    ><img className="col-lg-2 mt-5 noteTaker"
-                        src={noteTaker}
-                        alt="load out note taker page"
-                        /></a>
-                </div>
-                <div className="card text-decoration-none">
-                    <h3> Employee Tracker </h3>
-                    <a
-                        href="https://drive.google.com/file/d/1t4_gT6CboGx2IoG3NbFRhKzFjLYnywgl/view"
-                        target="no_blank"
-                    ><img className="col-lg-2 mt-5 tracker"
-                        src={tracker}
-                        alt="image of walk through"
-                        /></a>
-                </div>
-                <div className="card text-decoration-none">
-                    <h3>Team Profile Generator</h3>
-                    <a
-                        href="https://drive.google.com/file/d/1zqZZ6eW8WMP5yK_DuzPplQCe54emQCp3/view"
-                        target="no_blank"
-                    ><img className="col-lg-2 mt-5 team"
-                        src={team}
-                        alt="load out of website deployed"
-                        /></a>
-                </div>
-            </section >
-        </Link >
+        <div>
+            <h2>Work</h2>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={frogr}
+                    alt="load out of website deployed" />
+                <Card.Body>
+                    <Card.Title>Frogr</Card.Title>
+                    <Card.Text>
+                        This is a project I worked on with others to create a social media application.
+                    </Card.Text>
+                </Card.Body>
+                <ListGroup className="list-group-flush">
+                    <ListGroup.Item>Tech Used: </ListGroup.Item>
+                    <ListGroup.Item>I worked on the front end for this project using React and React bootstrap with custom Sass a collegue created.</ListGroup.Item>
+                    <ListGroup.Item>Contributers:</ListGroup.Item>
+                </ListGroup>
+                <Card.Body>
+                    <Card.Link href="https://frogr-app.herokuapp.com/" target="_blank">Site</Card.Link>
+                    <Card.Link href="https://github.com/cbaird21/frogr" target="_blank">Github</Card.Link>
+                </Card.Body>
+            </Card>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={frogger}
+                    alt="load out of website deployed" />
+                <Card.Body>
+                    <Card.Title>Hangman Game Application</Card.Title>
+                    <Card.Text>
+                        This is a project I worked on with others to recreate our childhood game of Hangman. See if you can win in the Wild West!
+                    </Card.Text>
+                </Card.Body>
+                <ListGroup className="list-group-flush">
+                    <ListGroup.Item>Tech Used: </ListGroup.Item>
+                    <ListGroup.Item>I worked on the front end for this project and also worked as the scrum master utilizing the kanban board.</ListGroup.Item>
+                    <ListGroup.Item>Contributers:</ListGroup.Item>
+                </ListGroup>
+                <Card.Body>
+                    <Card.Link href="https://crimekermitterhangman.herokuapp.com/" target="_blank">Site</Card.Link>
+                    <Card.Link href="https://github.com/cbaird21/Hangman_app" target="_blank">Github</Card.Link>
+                </Card.Body>
+            </Card>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={park}
+                    alt="load out of website deployed" />
+                <Card.Body>
+                    <Card.Title>National Park Finder</Card.Title>
+                    <Card.Text>
+                        This is a project I worked on with others to chose a National Park of your choice then get directions using Waze Api.
+                    </Card.Text>
+                </Card.Body>
+                <ListGroup className="list-group-flush">
+                    <ListGroup.Item>Tech Used: </ListGroup.Item>
+                    <ListGroup.Item>This was a project that we went through together, it was my first project in the industry.</ListGroup.Item>
+                    <ListGroup.Item>Contributers:</ListGroup.Item>
+                </ListGroup>
+                <Card.Body>
+                    <Card.Link href="https://heyitsbradley.github.io/National-Park-Finder-with-Directions/" target="_blank">Site</Card.Link>
+                    <Card.Link href="https://github.com/HeyItsBradley/National-Park-Finder-with-Directions" target="_blank">Github</Card.Link>
+                </Card.Body>
+            </Card>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={readMe}
+                    alt="load out of website deployed" />
+                <Card.Body>
+                    <Card.Title>ReadMe Generator</Card.Title>
+                    <Card.Text>
+                        I created this readMe generator to have higher quality readMe files.
+                    </Card.Text>
+                </Card.Body>
+                <ListGroup className="list-group-flush">
+                    <ListGroup.Item>Tech Used: </ListGroup.Item>
+                    <ListGroup.Item>This was a solo project.</ListGroup.Item>
+                </ListGroup>
+                <Card.Body>
+                    <Card.Link href="https://drive.google.com/file/d/1kmY9v3tVTNeo5AJn9NFcYaTgATlaDoSU/view" target="_blank">Walkthrough video</Card.Link>
+                    <Card.Link href="https://github.com/cbaird21/ReadMeGenerator" target="_blank">Github</Card.Link>
+                </Card.Body>
+            </Card>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={noteTaker}
+                    alt="load out of website deployed" />
+                <Card.Body>
+                    <Card.Title>Note Taker</Card.Title>
+                    <Card.Text>
+                        I created this note taker application to be able to create a todo list that I can add, edit and delete from.
+                    </Card.Text>
+                </Card.Body>
+                <ListGroup className="list-group-flush">
+                    <ListGroup.Item>Tech Used: </ListGroup.Item>
+                    <ListGroup.Item>This was a solo project.</ListGroup.Item>
+                </ListGroup>
+                <Card.Body>
+                    <Card.Link href="https://nottaker1.herokuapp.com/" target="_blank">Site</Card.Link>
+                    <Card.Link href="https://github.com/cbaird21/Note_Taker" target="_blank">Github</Card.Link>
+                </Card.Body>
+            </Card>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={tracker}
+                    alt="load out of website deployed" />
+                <Card.Body>
+                    <Card.Title>Employee Tracker</Card.Title>
+                    <Card.Text>
+                        I created this to be able to create a page with contact information, job title and group employees together for better tracking.
+                    </Card.Text>
+                </Card.Body>
+                <ListGroup className="list-group-flush">
+                    <ListGroup.Item>Tech Used: </ListGroup.Item>
+                    <ListGroup.Item>This was a solo project.</ListGroup.Item>
+                </ListGroup>
+                <Card.Body>
+                    <Card.Link href="https://drive.google.com/file/d/1t4_gT6CboGx2IoG3NbFRhKzFjLYnywgl/view" target="_blank">Walkthrough</Card.Link>
+                    <Card.Link href="https://github.com/cbaird21/Employee_Tracker" target="_blank">Github</Card.Link>
+                </Card.Body>
+            </Card>
+            <Card style={{ width: '18rem' }}>
+                <Card.Img variant="top" src={team}
+                    alt="load out of website deployed" />
+                <Card.Body>
+                    <Card.Title>Team Profile Generator</Card.Title>
+                    <Card.Text>
+                        I created this to be able to create a page with contact information, job title and group employees together for better tracking.
+                    </Card.Text>
+                </Card.Body>
+                <ListGroup className="list-group-flush">
+                    <ListGroup.Item>Tech Used: </ListGroup.Item>
+                    <ListGroup.Item>This was a solo project.</ListGroup.Item>
+                </ListGroup>
+                <Card.Body>
+                    <Card.Link href="https://drive.google.com/file/d/1zqZZ6eW8WMP5yK_DuzPplQCe54emQCp3/view" target="_blank">Walkthrough</Card.Link>
+                    <Card.Link href="https://github.com/cbaird21/Team_Profile_Generator" target="_blank">Github</Card.Link>
+                </Card.Body>
+            </Card>
+        </div >
     );
 }
