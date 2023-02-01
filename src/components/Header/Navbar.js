9d801b51e607b5f413ece79d922f72d159779952
unknown <chandler.baird21@gmail.com>
2023-02-01 11:32:42 -0700

    removed contact page but added react.mailto for email contact

--- src/components/Header/Navbar.js
+++ src/components/Header/Navbar.js
@@ -3,6 +3,14 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from "react-router-dom";
 
+function Mailto({ email, subject, body, ...props }) {
+    return (
+        <Nav.Link href={`mailto:${email}?subject=${subject || ""}&body=${body || ""}`}>
+            {props.children}
+        </Nav.Link>
+    );
+}
+
 export default function navbar() {
     return (
         <Navbar expand="lg" bg="dark" variant="dark">
@@ -13,12 +21,16 @@ export default function navbar() {
                     <Nav className="me-auto text-decoration-none">
                         <Link className="text-decoration-none m-2 text-light" to="/About"> About </Link>
                         <Link className="text-decoration-none m-2 text-light" to="/Project" > Projects </Link>
-                        <Link className="text-decoration-none m-2 text-light" to="/Contact" > Contact Me</Link>
                         <Link className="text-decoration-none m-2 text-light" to="/Resume" > Resume </Link>
                     </Nav>
                     <Nav>
                         <Nav.Link className="text-decoration-none m-2 text-light" href="https://github.com/cbaird21" target="blank">Github</Nav.Link>
                         <Nav.Link className="text-decoration-none m-2 text-light" href="https://www.linkedin.com/in/chandler-baird-39965b129/" target="blank">Linked In</Nav.Link>
+                        <Nav.Link href={`mailto:chandler.baird21@gmail.com`}>
+                            <Mailto email="chandler.baird21@gmail.com" subject="Contact Me" body="I'm looking forward to hearing from you... ">
+                                Email
+                            </Mailto>
+                        </Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
